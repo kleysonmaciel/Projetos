bb2b84d08a147afad4716b111a50a32377e4dc50
Kleyson Maciel <kleyson182@gmail.com>
2024-07-13 04:12:26 -0400

    Atualização nos Models

--- Venda-Produtos/Backend/Models/Usuario.cs
+++ Venda-Produtos/Backend/Models/Usuario.cs
@@ -1,10 +1,18 @@
-namespace MyApp.Models{
+namespace ProdutoWeb.Models{
     public class Usuario{
+        [key]
         public string id { get; set;}
+        [Required]
         public string name { get; set;}
+        [Required]
         public string sobrenome { get; set;}
+        [Required]
+        [EmailAddress]
         public string Email { get; set;}
+        [Rquired]
+        [StringLength(11)]
         public string cpf { get; set;}
+        [Required]
         public string senha_hash { get; set;}
     }
 }
