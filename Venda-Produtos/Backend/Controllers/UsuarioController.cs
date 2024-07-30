1d957ec956be45ce10ef5daa27638bce933dc9e1
Kleyson Maciel <kleyson182@gmail.com>
2024-07-30 02:02:03 -0400

    Atualizando Usuario Controller

--- Venda-Produtos/Backend/Controllers/UsuarioController.cs
+++ Venda-Produtos/Backend/Controllers/UsuarioController.cs
@@ -1,49 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using ProdutoWeb.Models;
 using ProdutoWeb.Services;
 
-namespace MyApp.Controllers{
-    [ApiController]
+namespace ProdutoWeb.Controllers{
     [Route("api/[controller]")]
+    [ApiController]
     public class UsuarioController : ControllerBase{
-        private readonly UsuarioService _usuarioService;
+        private readonly IUsuarioService _usuarioService;
 
-        public UsuarioController(UsuarioService usuarioService){
+        public UsuarioController(IUsuarioService usuarioService){
             _usuarioService = usuarioService;
         }
-        [HttpGet]
-        public ActionResult<List<Usuario>> GetAll() => _usuarioService.GetAll();
-
-        [HttpGet("{id}")]
-        public ActionResult<Usuario> Get(int id){
-            var Usuario = _usuarioService.Get(id);
-            if(Usuario == null){
-                return NotFound();
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] AuthenticateModel model){
+            var user = await _usuarioService.Authenticate(model.Username, model.Password);
+            if (user == null){
+                return BadRquest(new {message = "Email ou a senha estão incorretos"});
             }
-            return (Usuario);
+            
+            return Ok(user);
+            
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll(){
+            var users = await _usuarioService.GetAll();
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id){
+            var users = await _usuarioService.GetById(id);
+            return Ok(users);
         }
         [HttpPost]
-        public IActionResult Create(Usuario usuario){
-            _usuarioService.Create(usuario);
-            return CreatedAtAction(nameof(Get), new {id = usuario.Id}, usuario);
-        }
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, Usuario usuario){
-            var existingUsuario = _usuarioService.Get(id);
-            if(existingUsuario == null){
-                return NotFound();
+        public async Task<IActionResult> Create([FromBody] CreateModel model){
+            var user = new Usuario{
+                nome = model.nome,
+                sobrenome = model.sobrenome,
+                email = model.email,
+                cpf = model.cpf
+            };
+            try{
+                await _usuarioService.Create(user, model.Password);
+                return Ok(user);
             }
-            _usuarioService.Update(id, usuario);
-            return NoContent();
-        }
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id){
-            var usuario = _usuarioService.Get(id);
-            if(usuario == null){
-                return NotFound();
+            catch(Exception ex){
+                return BadRquest(new{message = ex.Message});
             }
-            _usuarioService.Delete(id);
-            return NoContent();
         }
+        
     }
 }
