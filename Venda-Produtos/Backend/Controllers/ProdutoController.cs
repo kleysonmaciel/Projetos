7c45875bc5f5d96f3fbfc259fa1aae09c894e13b
Kleyson Maciel <kleyson182@gmail.com>
2024-07-30 03:18:17 -0400

    Corrigindo arquivo Produto Controller

--- Venda-Produtos/Backend/Controllers/ProdutoController.cs
+++ Venda-Produtos/Backend/Controllers/ProdutoController.cs
@@ -1,48 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using ProdutoWeb.Models;
 using ProdutoWeb.Services;
 
-namespace MyApp.Controllers{
+namespace ProdutoWeb.Controllers{
     [ApiController]
     [Route("api/[controller]")]
-    public class ProdutoController : ControllerBase{
-        private readonly ProdutoService _produtoService
-        public ProdutoController(ProdutoService produtoService){
-            _produtoService = produtoService;
-        }    
-        [HttpGet]
-        public ActionResult<List<Produto>> GetAll() => _produtoService.GetAll();
-
-        [HttpGet("{id}")]
-        public ActionResult<Produto> Get(int id){
-            var produto = _produtoService.Get(id);
-            if (produto == null){
-                return NotFound();
-            }
-            return produto;
-        }
-        [HttpPost]
-        public IActionResult Create(Produto produto){
-            _produtoService.Create(produto);
-            return CreatedAtAction(nameof(Get), new {id = produto.Id}, produto);
-        }
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, Produto produto){
-            var existingProduto = _produtoService.Get(id);
-            if(existingProduto == null){
-                return NotFound();
-            }
-            _produtoService.Update(id, produto);
-            return NoContent();
-        }
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id){
-            var produto = _produtoService.Get(id);
-            if(produto == null){
-                return NotFound();
-            }
-            _produtoService.Delete(id);
-            return NoContent();
-        }
-    }
+   
 }
