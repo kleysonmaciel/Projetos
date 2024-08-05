163cb201e46865277ba01ec9d7ee916f2f2474de
Kleyson Maciel <kleyson182@gmail.com>
2024-08-05 18:31:45 -0400

    Adicionando ConfirmAction.vue do Componets

---
+++ Venda-Produtos/frontend/src/components/ConfimAction.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Cobnfirmar Ação</h2>
+        <p>{{ message }}</p>
+        <button @click="confirmAction">Confirmar</button>
+        <button @click="cancelAction">Cancelar</button>
+    </div>
+</template>
+
+<script>
+export default{
+    props:{
+        message: {
+            type: String,
+            required: true
+        },
+        confirmAction:{
+            type: Function,
+            required: true
+        },
+        cancelAction:{
+            type: Function,
+            required: true
+        }
+    }
+};
+</script>
