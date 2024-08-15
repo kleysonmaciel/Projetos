01687b99937140d661e2c023186564160f718d7f
Kleyson Maciel <kleyson182@gmail.com>
2024-08-15 13:40:10 -0400

    Adicionando app.vue main.js ao projeto

---
+++ Venda-Produtos/frontend/src/app.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import router from './router';
+</script>
+
+<template>
+    <div id="app">
+        <router-view />
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'App'
+};
+</script>
+
+<style>
+body{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+</style>
