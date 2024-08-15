dfaffabcb7d71cfdfd4d2213c3b6b24d331f7d7c
Kleyson Maciel <kleyson182@gmail.com>
2024-08-15 13:41:18 -0400

    Corrigindo bug no app.vue no projeto

--- Venda-Produtos/frontend/src/app.vue
+++ Venda-Produtos/frontend/src/app.vue
@@ -1,24 +1,20 @@
-<script setup lang="ts">
-import router from './router';
-</script>
-
 <template>
     <div id="app">
-        <router-view />
+      <router-view />
     </div>
-</template>
-
-<script>
-
-export default{
+  </template>
+  
+  <script>
+  export default {
     name: 'App'
-};
-</script>
-
-<style>
-body{
+  };
+  </script>
+  
+  <style>
+  body {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     margin: 0;
     padding: 0;
-}
-</style>
+  }
+  </style>
+  
