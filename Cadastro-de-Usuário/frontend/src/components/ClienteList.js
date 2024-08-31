794aeee06b68e7f907b48faeee32f3e60cb390c5
kleysonmaciel <kleyson182@gmail.com>
2024-08-31 08:43:12 -0400

    Alterando algumas configurações de para usar rotas de edição

--- Cadastro-de-Usuário/frontend/src/components/ClienteList.js
+++ Cadastro-de-Usuário/frontend/src/components/ClienteList.js
@@ -1,32 +1,54 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 
 function ClienteList(){
-    const [clientes, setClientes] = useState([]);
-    useEffect(()=>{
+
+    const [loading, setLoading] = useState(true);
+
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetchClientes();
+    }, []);
+
+    const fetchClientes = () => {
         axios.get('/api/cliente')
-        .then(response => {
-            setClientes(response.data);
-        });
-    },[]);
-    const deleteCliente = (id)=> {
-        axios.delete(`/api/cliente/${id}`)
-        .then(() =>{
-            setClientes(clientes.filter(cliente => cliente.id !== id));
-        });
+            .then(response => {
+                setClientes(response.data);
+            })
+            .catch(err => {
+                setError('Erro ao buscar clientes');
+            });
     };
-    return(
-      <div>
-        <h2>Lista de Clientes</h2>
-        <ul>
-            {clientes.map(cliente => (
-                <li key={cliente.id}>
-                    {cliente.nome}{cliente.sobrenome} - {cliente.email}
-                    <buttoen onClick={()=> deleteCliente(cliente.id)}>Excluir</buttoen>
-                </li>
-            ))}
-        </ul>
-      </div>
+
+    const deleteCliente = (id) => {
+        axios.delete(`/api/cliente/${id}`)
+            .then(() => {
+                setClientes(clientes.filter(cliente => cliente.id !== id));
+            })
+            .catch(err => {
+                setError('Erro ao deletar cliente.');
+            });
+    };
+
+    if (error) return <p>{Error}</p>;
+
+    return (
+        <div>
+            <h2>Lista de Clientes</h2>
+            <Link to="/adicionar"><button>Adicionar Cliente</button></Link>
+            <ul>
+                {clientes.map(cliente => (
+                    <li key={cliente.id}>
+                        {cliente.nome} {cliente.sobrenome} - {cliente.email}
+                        <button onClick={() => deleteCliente(cliente.id)}>Excluir</button>
+                        <Link to={`/editar/${cliente.id}`}><button>Editar</button></Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 export default ClienteList;
