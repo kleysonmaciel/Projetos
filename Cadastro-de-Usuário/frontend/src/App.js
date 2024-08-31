2ab0c70e1d3484428a4ece11d541ea00c8974d2b
kleysonmaciel <kleyson182@gmail.com>
2024-08-31 08:45:34 -0400

    Adicionando as rotas

--- Cadastro-de-Usuário/frontend/src/App.js
+++ Cadastro-de-Usuário/frontend/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ClienteList from './components/ClienteList';
 import ClienteForm from './components/ClienteForm';
+import ClienteEdit from './components/ClienteEdit';
 
 function App(){
     return(
+        <Router>
         <div className="App">
-            <h1>Cadastro de Clientes</h1>
-            <ClienteForm/>
-            <ClienteList/>
+          <h1>Cadastro de Clientes</h1>
+          <Routes>
+            <Route path="/" element={<ClienteList />} />
+            <Route path="/adicionar" element={<ClienteForm />} />
+            <Route path="/editar/:id" element={<ClienteEditWrapper />} />
+          </Routes>
         </div>
+      </Router>
     );
 }
-
+function ClienteEditWrapper(){
+    const {id} = useParams();
+    return <ClienteEdit id={id} />
+}
 export default App;
